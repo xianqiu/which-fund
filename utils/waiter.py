3faa2d4de4dc5b50c05ce085edd79989f58ef217
xqiu <x.qiu@qq.com>
2025-02-24 16:19:43 +0800

    staging

---
+++ utils/waiter.py
@@ -0,0 +1,12 @@
+
+class Scheduler(object):
+
+    def __init__(self, n):
+        """ 把 span 划分成 k
+        :param span: int, 代表总时长, 单位: 秒
+        :param k: 次数
+        """
+        self.time = time
+        self.k = k
+
+    def sleep(self):
